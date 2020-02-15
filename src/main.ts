4bc0b083443c918c44bf9ce01da4abbd684c2da4
kawamataryou <ba068082@gmail.com>
2020-02-16 07:21:34 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import VueCompositionApi from "@vue/composition-api";
+
+Vue.config.productionTip = false;
+
+Vue.use(VueCompositionApi);
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
