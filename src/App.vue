fd6a15936ccbabaa64e687e6054603a0a9243651
kawamataryou <ba068082@gmail.com>
2020-02-16 07:41:57 +0900

    Init vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-app>
+    <v-content>
+      <h1>hello</h1>
+      <v-btn>ok vuetify</v-btn>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "App",
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    //
+  })
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
